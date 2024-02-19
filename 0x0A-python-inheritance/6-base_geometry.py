0750c4d8a789ee1c4a6c92b3974f24589ecf7922
Laurenmsoo <lmugwiria13@gmail.com>
2024-02-19 15:07:24 -0800

    class with method that raises exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+5-base_geometry class module
+"""
+
+
+class BaseGeometry:
+    """class with a public instance method, area"""
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
