d4bd86e9456ba01244f1e56aec20ffb4582aa240
Laurenmsoo <lmugwiria13@gmail.com>
2024-02-20 15:06:05 -0800

    reads a file and prints to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+reads file
+"""
+
+def read_file(filename=""):
+    """reads a and prints it to stdout
+    Returns none
+    """
+    with open(filename,"r",encoding="UTF8") as f:
+        print(f.read(), end ="")
