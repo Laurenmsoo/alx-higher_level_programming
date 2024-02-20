98edec2301f90dc33b64052908abf19a6ce450c0
Laurenmsoo <lmugwiria13@gmail.com>
2024-02-20 15:33:48 -0800

    appends to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+"""
+Appends to a file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    appends a string at the end of file and
+    returns the number of characters added
+    """
+    with open(filename, "a", encoding="UTF8") as f:
+        return f.write(text)
