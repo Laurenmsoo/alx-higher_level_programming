9306ccb37fd2c8f850389f06e8df2727c14c2ac2
Laurenmsoo <lmugwiria13@gmail.com>
2024-02-20 16:04:04 -0800

    returns obj rep by a string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" function that returns an object represented by a JSON string
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    reurns obj rep by JSON string
+    """
+    return json.loads(my_str)
