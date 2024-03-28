7fcb5a326b5ddfb7b48968624ad014a797c2dcba
Laurenmsoo <lmugwiria13@gmail.com>
2024-03-27 19:14:39 -0700

    lists all shows, and all genres linked to that show, from the database

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,9 @@
--- Lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+-- Lists all shows, and all genres linked to that show, from the database.
 
-SELECT tv_shows.title,
-	tv_genres.name
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-	LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-	LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title ASC,
-	tv_genres.name ASC;:
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
