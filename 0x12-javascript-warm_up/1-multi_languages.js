cfff3917c08af126eb40f0ab8847f90132334012
Laurenmsoo <lmugwiria13@gmail.com>
2024-04-26 03:31:23 -0700

    print 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
-const name = ['C is fun','Python is cool','JavaScript is amazing'];
-for (let i in name){
-	console.log(name[i]);
-}
+const name = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const i in name) { console.log(name[i]); }
